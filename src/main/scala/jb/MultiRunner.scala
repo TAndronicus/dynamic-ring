22865d0fb4933558d7e13c4a94cc6a837fdc3857
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2021-04-16 00:05:17 +0200

    Read the names of the files

--- src/main/scala/jb/MultiRunner.scala
+++ src/main/scala/jb/MultiRunner.scala
@@ -1,43 +1,19 @@
 package jb
 
 import jb.conf.Config
+import jb.util.Const
 import jb.util.Const.FILENAME_PREFIX
 import jb.util.result.{GeneralCatcher, ResultCatcher}
+
+import java.io.File
 
 object MultiRunner {
 
 
   def run(nClassif: Int, nFeatures: Int): Unit = {
-    val filenames = Array(
-//      "aa",
-      "ap",
-      "ba",
-      "bi",
-      "bu",
-      "c",
-      "d",
-      "ec",
-      "h",
-      "i",
-      "ir",
-      "m",
-      "ma",
-      "p",
-      "ph",
-      "pi",
-      "ri",
-      "sb",
-      "se",
-      "t",
-      "te",
-      "th",
-      "ti",
-      "wd",
-      "wi",
-      "wr",
-      "ww",
-      "ye"
-    )
+    val filenames = new File(Const.FILENAME_PREFIX)
+      .listFiles()
+      .map(_.getName)
 
     val runner = new Runner(nClassif, nFeatures)
     val resultCatcher = runForFiles(runner)(filenames)
@@ -48,16 +24,12 @@ object MultiRunner {
   private def runForFiles(runner: Runner)(filenames: Array[String]): ResultCatcher = {
     val resultCatcher = getResultCatcher
     while (resultCatcher.canConsume && !resultCatcher.isFull) {
-      //      try {
       val scores = new Array[Array[Double]](filenames.length)
       for (index <- filenames.indices) {
         println(filenames(index))
         scores(index) = runner.calculateMvIScores(FILENAME_PREFIX + filenames(index))
       }
       resultCatcher.consume(scores)
-      //      } catch {
-      //        case e: Throwable => println("Caught " + e.getMessage)
-      //      }
     }
     resultCatcher
   }
